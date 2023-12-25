35ab49764c55fb541937be98f6b8bbf692ce9440
SantiRimedio <116693154+SantiRimedio@users.noreply.github.com>
2023-12-25 16:27:27 -0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -17,8 +17,11 @@ with open(r"models/xgboost_best_hedonic_model.pkl", 'rb') as file:
 # Titulo
 st.title("Valuación de Departamentos")
 
+# Input Controls Section
+st.sidebar.header("Parámetros de la Propiedad")
+
 # Tomo la ubicación
-location_text = st.text_input("Dirección de la propiedad", "Buenos Aires, Argentina")
+location_text = st.sidebar.text_input("Dirección de la propiedad", "Buenos Aires, Argentina")
 
 try:
     # Georeferencio la ubicación
@@ -31,33 +34,36 @@ try:
         Latitud, Longitud = 0.0, 0.0
 except:
     st.error(f"Error: {location_text}. La ubicación es incorrecta.")
-        
 
-# Ploteo la ubicación ingresada
+# Property Details Section
+st.sidebar.header("Detalles de la Propiedad")
+
+PropiedadSuperficieTotal = st.sidebar.number_input("Superficie Total (m²)", min_value=0, max_value=500, value=50)
+Antiguedad = st.sidebar.number_input("Antiguedad", min_value=0, max_value=100, value=0)
+CantidadDormitorios = st.sidebar.slider("Cantidad de Ambientes (Dormitorios + 1)", min_value=1, max_value=10, value=2)
+
+# Prediction Section
+st.sidebar.header("Resultados de la Valuación")
+
+# Prediction Button
+if st.sidebar.button("Calcular Valuación"):
+    with st.spinner("Realizando la predicción..."):
+        # Predicción
+        features = np.array([[Latitud, Longitud, PropiedadSuperficieTotal, CantidadDormitorios, Antiguedad, 2023]])
+
+        try:
+            features = scaler.transform(features)
+            prediction = loaded_model.predict(features)[0]
+
+            # Display prediction with styling
+            st.success(f"Valor Estimado: ${prediction:,.2f}")
+
+        except Exception as e:
+            st.error(f"Error durante la predicción: {e}")
+            prediction = 0.0  # or any default value
+
+# Map Display Section
+st.header("Ubicación de la Propiedad")
 data = [{'LATITUDE': Latitud, 'LONGITUDE': Longitud}]
 df = pd.DataFrame(data)
-st.map(data=df,latitude="LATITUDE", longitude="LONGITUDE", zoom=15, size=10)
-
-# Ingreso los otros datos
-PropiedadSuperficieTotal = st.number_input("Metros cuadrados", min_value=0, max_value=500, value=50)
-
-Antiguedad = st.number_input("Antiguedad", min_value=0, max_value=100, value=0)
-
-CantidadDormitorios = st.slider("Cantidad de Ambientes", min_value=1, max_value=10, value=2) + 1
-
-AÑO = 2023
-
-# Predicción
-features = np.array([[Latitud, Longitud, PropiedadSuperficieTotal, CantidadDormitorios, Antiguedad, AÑO]])
-features = scaler.transform(features)
-prediction = loaded_model.predict(features)[0]
-
-# Display prediction
-low_limit = 0.9 * prediction
-high_limit = 1.1 * prediction
-
-st.markdown(
-    f"<h2 style='text-align: center; font-family: Arial, sans-serif;'>La valuación de la propiedad está entre el rango de: ${low_limit:,.2f} <span style='color: black;'>y</span> ${high_limit:,.2f}</h2>",
-    unsafe_allow_html=True
-)
-#%%
+st.map(data=df, latitude="LATITUDE", longitude="LONGITUDE", zoom=15, size=10)
